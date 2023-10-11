85935c417eef778ea6a3aa6ef17214e760285409
Khushal Todi <93736238+geekyboy18@users.noreply.github.com>
2023-10-11 16:21:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Hello!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require("express");
+const app = express();
+app.all("/", (req, res) => {
+	console.log("Just got a request!");
+	res.send(`<html>
+	<head>
+		<script type="text/javascript" src="http://projects8-dev-ed.develop.my.salesforce.com/support/api/59.0/lightning/opencti_min.js"></script>
+		<script type="text/javascript">
+			var callback = function (response) {
+				if (response.success) {
+					console.log("API method call executed successfully! returnValue:", response.returnValue);
+				} else {
+					console.error("Something went wrong! Errors:", response.errors);
+				}
+			};
+			function searchAndScreenPop() {
+				//Invokes API method
+				sforce.opencti.searchAndScreenPop({ searchParams: "Acme", queryParams: "Key1=value1&Key2=value2", callType: sforce.opencti.CALL_TYPE.INBOUND, deferred: false, callback: callback });
+			}
+		</script>
+	</head>
+	<body>
+		<button onclick="searchAndScreenPop();">searchAndScreenPop</button>
+	</body>
+</html>
+`);
+});
+app.listen(process.env.PORT || 3000);

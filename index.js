baa8903dad4d4c6fd06b64ab8168d891e5f71053
Khushal Todi <khushaltodi@cyntexa.com>
2023-10-13 12:19:58 +0530

    Update

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const app = express();
 app.use(express.static(__dirname));
 app.all("/", (req, res) => {
 	console.log("Just got a request!");
-	res.send(`<html>
-<head>
+	res.send(
+		`<html>
+    <head>
    <script type="text/javascript" src="./script.js"></script>
    <script type="text/javascript">
       	 var callback = function(response) {
@@ -16,13 +17,20 @@ app.all("/", (req, res) => {
       	};
        function searchAndScreenPop() {
                 //Invokes API method
-                sforce.opencti.searchAndScreenPop({ searchParams : 'Acme',queryParams : 'Key1=value1&Key2=value2', callType : sforce.opencti.CALL_TYPE.INBOUND,  deferred: false, callback : callback });
+                // queryParams : 'Key1=value1&Key2=value2',
+                sforce.opencti.searchAndScreenPop({ 
+                    searchParams : 'Burlington Textiles Corp of America',
+                    callType : sforce.opencti.CALL_TYPE.INBOUND,
+                    deferred: false,
+                    callback : callback 
+                });
         }
     </script>
-</head>
-<body>
-    <button onclick="searchAndScreenPop();">searchAndScreenPop</button>
-</body>
-</html>`);
+    </head>
+    <body>
+        <button onclick="searchAndScreenPop();">searchAndScreenPop</button>
+    </body>
+    </html>`
+	);
 });
 app.listen(process.env.PORT || 3000);
